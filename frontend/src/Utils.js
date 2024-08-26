86a72a14381170939fa9217b8decee274fb7ba7e
nayakbiku22 <116290958+nayakbiku22@users.noreply.github.com>
2024-08-26 12:32:25 +0530

    Update Utils.js

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -1,5 +1,5 @@
 import { toast } from "react-toastify";
-export const API_URL="http://localhost:8080";
+export const API_URL="https://todo-api-pearl-seven.vercel.app";
 export const notify=(message,type)=>{
     toast[type](message)
 }
